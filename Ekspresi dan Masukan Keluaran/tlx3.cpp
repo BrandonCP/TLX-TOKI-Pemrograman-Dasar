fdddfa2f98c48d33ddbe908ae6d2e4be470596e2
BrandonCP <63950467+BrandonCP@users.noreply.github.com>
2020-04-19 17:03:55 +0700

    Add files via upload

---
+++ Ekspresi dan Masukan Keluaran/tlx3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string a; // declare string variable
+    getline(cin, a); // input a line of command or order 
+    cout << a << endl; // output the line of command that inputed before 
+}
---
+++ Ekspresi dan Masukan Keluaran/tlx4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B; // declare two integer variables
+    cin >> A >> B; // input variable A and then input variable B
+    cout << "masing-masing " << A/B << endl; // output masing-masing [A divided by B]
+    cout << "bersisa " << A%B << endl; // output bersisa [rest value of A divided by B]
+}
