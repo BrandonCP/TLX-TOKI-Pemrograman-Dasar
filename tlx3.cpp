407b21cd8a8fb9d20c62f880ea10dfacbdbad2e1
BrandonCP <63950467+BrandonCP@users.noreply.github.com>
2020-04-19 17:05:56 +0700

    Delete tlx3.cpp

--- tlx3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    string a; // declare string variable
-    getline(cin, a); // input a line of command or order 
-    cout << a << endl; // output the line of command that inputed before 
-}
