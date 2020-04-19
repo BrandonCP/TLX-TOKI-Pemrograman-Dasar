e554abc98f767f192b47cbc86eb35687a44abc9f
BrandonCP <63950467+BrandonCP@users.noreply.github.com>
2020-04-19 17:11:44 +0700

    Delete tlx12.cpp

--- tlx12.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-int main() {
-    int x1, x2, y1, y2;
-    cin >> x1 >> y1 >> x2 >> y2;
-    cout << abs(x1-x2) + abs(y1 - y2) << endl; // cari nilai absolut dari |x1-x2| + |y1-y2|
-}
